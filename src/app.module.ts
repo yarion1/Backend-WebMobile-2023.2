f78fb669d5505c3c7d017a6616975855d942a7d4
KharlosDanyell <oliveirakharlos@gmail.com>
2023-11-20 13:34:34 -0300

    [webm-0001] - endpoint de login e registro de usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import {AppService} from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {TypeOrmConfigService} from './config/typeorm.config';
 import {ConfigModule} from "@nestjs/config";
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
     imports: [ConfigModule.forRoot(), TypeOrmModule.forRootAsync({
         imports: [ConfigModule],
         useClass: TypeOrmConfigService,
-    })],
+    }), AuthModule, UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ src/auth/constants.ts
@@ -0,0 +1,6 @@
+import { JWT_SECRET } from "src/config/JWT_SECRET";
+
+export const jwtConstants = {
+  secret: 'bs&hUhg$2783@M15_fd',
+  expiresIn: '1d',
+};
