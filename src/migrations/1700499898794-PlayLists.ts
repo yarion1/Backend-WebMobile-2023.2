973a15c92095950202dc3389ac254bb5f2f15433
KharlosDanyell <oliveirakharlos@gmail.com>
2023-11-21 08:59:05 -0300

    tabela playlists]

---
+++ src/migrations/1700499898794-PlayLists.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm"
+
+export class PlayLists1700499898794 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'playlists',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    { name: 'name', type: 'varchar', isNullable: false },
+                    {name: 'contents_id', type: 'json', isNullable: false},
+                    {name: 'user_id', type: 'integer', isNullable: false},
+                    { name: 'created_at', type: 'timestamp', default: 'now()' },
+                    { name: 'updated_at', type: 'timestamp', default: 'now()' },
+                ],
+            }),
+        )
+
+        await queryRunner.createForeignKey(
+            'playlists',
+            new TableForeignKey({
+                columnNames: ['user_id'],
+                referencedTableName: 'users',
+                referencedColumnNames: ['id'],
+                onDelete: 'CASCADE',
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('playlists', 'FK_user_id');
+        await queryRunner.dropTable('playlists');
+    }
+}
