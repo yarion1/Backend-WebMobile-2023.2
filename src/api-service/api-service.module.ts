bc2e95a83be7db228f90190b5aa47314d8849f57
KharlosDanyell <oliveirakharlos@gmail.com>
2023-11-25 10:01:39 -0300

    feature/webm-0007

--- src/api-service/api-service.module.ts
+++ src/api-service/api-service.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ApiServiceService } from './api-service.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule],
   providers: [ApiServiceService],
   exports: [ApiServiceService],
 })
